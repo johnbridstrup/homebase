935346312254a6c26e6512e3269e6cb5474a4f77
John Bridstrup <john.bridstrup@gmail.com>
2024-06-19 16:44:16 -0700

    admin for sensors

--- homebase/sensors/admin.py
+++ homebase/sensors/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Sensor, SensorData
+
+
+class SensorAdmin(admin.ModelAdmin):
+    list_display = ("name", "room", "sensor_type", "identifier", "status", "last_seen")
+    search_fields = (
+        "name",
+        "room__name",
+    )
+
+
+class SensorDataAdmin(admin.ModelAdmin):
+    list_display = ("sensor", "data", "timestamp")
+    search_fields = (
+        "sensor__name",
+    )
+
+admin.site.register(Sensor, SensorAdmin)
+admin.site.register(SensorData, SensorDataAdmin)
